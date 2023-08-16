deb405a02b89a72a877fec72ec304d87fea0c5c2
Alexandru Ursu <bodhipankh@yahoo.com>
2023-08-16 00:36:09 +0000

    Generate test for test_models.py

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,4 +1,10 @@
 from django.test import TestCase
+from .models import Item
 
 # Create your tests here.
 
+class TestModels(TestCase):
+
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
